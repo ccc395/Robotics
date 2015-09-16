1d4bd947bea3c8e58c0a26de38e8f40f6b6e145a
Christopher <ccc395@vt.edu>
2015-09-15 23:09:42 -0400

    Commit description Description Commit

--- quadrature.py
+++ quadrature.py
@@ -16,22 +16,30 @@ class QuadratureEstimator:
   def update(self, a_state, b_state, time):
     # Implement decoding here. Note you'll remove the pass statement once you start implementing this
       while True
+      #sets up where the initial velocity and position is not calculated as it cannot be
         if count != 0:
-
+          #if moving forward
           if ((a_state, b_state_old) == (1,0)) or ((a_state,b_state_old) ==(0,1)):
-            
+            #declares the direction of travel is forwards
             direction = "+"
+            #declares the location of each pulse
             if (a_state, a_state_old, b_state) == (1,0,0)
               pulcount = pulcount + 1
+            #calculates the velocity
             vel = (((pulcount/ticks_per_revolution) * 60)/(time - time_old))
+          #if moving backward
           elif((a_state,b_state_old == (1,1))) or ((a_state,b_state_old) == (0,0)):
+            #declares the direction of travel is reverse
             direction = "-"
+            #declares the location of each pulse
             if (a_state, a_state_old, b_state) == (1,0,0)
               pulcount = pulcount + 1
+            #calculates the velocity
             vel = (((pulcount/ticks_per_revolution) * 60)/(time - time_old))
           else 
+            #stores the values for when there is no pulse or seperate tic
             trash = trash + 1
-
+          #sets the current states as the old states
           a_state = a_state_old
           b_state = b_state_old
         else
